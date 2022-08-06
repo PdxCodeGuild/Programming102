114c9db181586056c0cbfadb5def6da3697b8719
Jamesjr95 <92341570+Jamesjr95@users.noreply.github.com>
2022-08-05 18:09:07 -0700

    Update web_api_template.py

--- resources/example_lessons/unit_5/web_api_template.py
+++ resources/example_lessons/unit_5/web_api_template.py
@@ -2,13 +2,26 @@
 
 # requests module is used to make HTTP requests to websites
 
-# url to access a list of todo items
-# url to access a single todo item
+
+
+# ask user for their search term
+
+
+
+# url to access a list of dad jokes
+
+
 
 # make an HTTP GET request to the url and save the HTTP response in a variable
+
+
+
 
 # JSON - JavaScript Object Notation (JS's version of a dictionary)
 # convert the response data from JSON to Python dictionary
 # using the .json() method of the response object
+
+
+
 
 # display the data
